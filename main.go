e7ad13f55e515594f8991cf0aafdbde286ee0b7d
khoand10 <khoand1011@gmail.com>
2024-02-29 18:01:28 +0700

    add http server

--- main.go
+++ main.go
@@ -3,28 +3,51 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"go.uber.org/fx"
+	"net"
+	"net/http"
 	"os"
 )
 
 type (
-	Store interface {
+	Connection string
+	Store      interface {
 		Get(string) string
 	}
 
-	MysqlStore struct{}
+	MysqlStore struct {
+		connection Connection
+	}
 
 	Service struct {
 		store Store
 	}
 )
 
+func NewConnection() Connection {
+	return "mysql"
+}
+
+func NewRouter() *gin.Engine {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+	return r
+}
+
 func (s *MysqlStore) Get(data string) string {
 	return "Mysql: " + data
 }
 
-func NewMysqlStore() Store {
-	return &MysqlStore{}
+func NewMysqlStore(con Connection) Store {
+	return &MysqlStore{
+		connection: con,
+	}
 }
 
 func NewService(
@@ -35,13 +58,36 @@ func NewService(
 	}
 }
 
+func NewHTTPServer(lc fx.Lifecycle, gin *gin.Engine) *http.Server {
+	srv := &http.Server{Addr: ":8080", Handler: gin}
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			ln, err := net.Listen("tcp", srv.Addr)
+			if err != nil {
+				return err
+			}
+			fmt.Println("Starting HTTP server at", srv.Addr)
+			go srv.Serve(ln)
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return srv.Shutdown(ctx)
+		},
+	})
+	return srv
+}
+
 var ServiceModule = fx.Module("service",
+	fx.Provide(NewConnection),
 	fx.Provide(NewMysqlStore),
 	fx.Provide(NewService),
+	fx.Provide(NewHTTPServer),
+	fx.Provide(NewRouter),
 	fx.Invoke(func(s *Service) {
 		rs := s.store.Get("hello")
 		fmt.Println(rs)
 	}),
+	fx.Invoke(func(server *http.Server) {}),
 )
 
 func main() {
